89c37ffdf30d101125e6dc17fe9f40de1367eb76
urj2258 <liaqtumer296@gmail.com>
2023-10-22 22:14:49 +0500

    promt user

--- hack2.c
+++ hack2.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
+#include<math.h>
+
 int main(){
-    double lat1,lan1,lat2lon2;
-    
+
+    double lat1,lon1,lat2,lon2;
+
+    printf("Enter the coordinates of the origin:\n");
+    printf("Latitude (in degrees): ");
+    scanf("%lf",&lat1);
+    printf("Latitude (in degrees): ");
+    scanf("%lf",&lon1);
 }
